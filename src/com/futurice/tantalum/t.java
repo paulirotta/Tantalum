6114a687a7d1651ea8afb8e5b711e5fdb059d760
phou@FL146.local <phou@FL146.local>
2011-09-26 09:58:18 +0300

    test

---
+++ src/com/futurice/tantalum/t.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.futurice.tantalum;
+
+/**
+ *
+ * @author phou
+ */
+public class t {
+    
+}
