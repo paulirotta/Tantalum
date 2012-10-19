3828f1bc98bab4ac5a11f47a9fcc2bc9f3086fd3
paul.houghton@futurice.com <paul.houghton@futurice.com>
2012-10-19 14:20:16 +0300

    urlEncode and urlDecode in StringUtils are re-written, more general and bigger coverage of UTF-8 codebase. Testing needed

--- Tantalum4Library/build/UsbDebug/preprocessed/com/futurice/tantalum4/util/StringUtils.java
+++ Tantalum4Library/build/UsbDebug/preprocessed/com/futurice/tantalum4/util/StringUtils.java
@@ -1,6 +1,6 @@
 package com.futurice.tantalum4.util;
 
-import java.io.ByteArrayOutputStream;
+import com.futurice.tantalum4.log.L;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Vector;
@@ -13,6 +13,7 @@ import javax.microedition.lcdui.Font;
  */
 public class StringUtils {
 
+    private final static String UNRESERVED_CHARS = ".-_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ*~";
     private static StringUtils singleton;
     private final static String ELIPSIS = "...";
 
@@ -124,22 +125,116 @@ public class StringUtils {
         return new String(readBytesFromJAR(name));
     }
 
+    /**
+     * UTF-8 URL decode of data received from a web service
+     * 
+     * @param in
+     * @return 
+     */
     public static String urlDecode(final String in) {
         final char[] chars = new char[in.length()];
-        final ByteArrayOutputStream out = new ByteArrayOutputStream(in.length());
+        final StringBuffer sb = new StringBuffer(in.length());
 
         in.getChars(0, in.length(), chars, 0);
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == '+') {
-                out.write(' ');
+                sb.append(' ');
             } else if (chars[i] == '%') {
-                out.write((char) ((Character.digit(chars[++i], 16) << 4) + Character.digit(chars[++i], 16)));
+                int c = ((Character.digit(chars[++i], 16) << 4) + Character.digit(chars[++i], 16));
+                if (c >= 0x7F) {
+                    // multi-byte
+                    int byteCount = 0;
+                    int c2 = c;
+                    while ((c2 & 80) != 0) {
+                        byteCount++;
+                        c2 <<= 1;
+                    }
+                    if (i + 3 * byteCount >= chars.length) {
+                        L.i("Illegal urlDecode, string not long enough for multi-byte decode", in + " : index=" + i);
+                    } else {
+                        switch (byteCount) {
+                            case 2:
+                                c &= 0x1F;
+                                c <<= 6;
+                                // #xx second digit
+                                c2 = ((Character.digit(chars[++i], 16) << 4) + Character.digit(chars[++i], 16));
+                                c2 &= 0x3F;
+                                c |= c2;
+                                break;
+                            case 3:
+                                c &= 0x0F;
+                                c <<= 12;
+                                // #xx second digit
+                                c2 = ((Character.digit(chars[++i], 16) << 4) + Character.digit(chars[++i], 16));
+                                c2 &= 0x3F;
+                                c |= c2 << 6;
+                                // #xx third digit
+                                c2 = ((Character.digit(chars[++i], 16) << 4) + Character.digit(chars[++i], 16));
+                                c2 &= 0x3F;
+                                c |= c2;
+                                break;
+                            default:
+                                L.i("Unsupported urlDecode, bytes=" + byteCount, in + " : index=" + i);
+                        }
+                    }
+                }
+                sb.append(c);
             } else {
-                out.write(chars[i]);
+                sb.append(chars[i]);
             }
         }
 
-        return out.toString();
+        return sb.toString();
+    }
+    
+    /**
+     * UTF-8 url encode data before submitting to a web service
+     * 
+     * @param in
+     * @return 
+     */
+    public static String urlEncode(final String in) {
+        final StringBuffer sb = new StringBuffer(10 + in.length() * 5 / 4);
+        final int l = in.length();
+        
+        for (int i = 0; i < l; i++) {
+            int c = in.charAt(i);
 
+            if (UNRESERVED_CHARS.indexOf(c) >= 0) {
+                sb.append(c);
+            } else if (c == ' ') {
+                sb.append('+');
+            } else if (c < 0x80) {
+                // single byte
+                hexEncode(c, sb);
+            } else if (c < 0x800) {
+                // two byte
+                hexEncode(0x60 | (c >>> 6), sb);
+                hexEncode(0x80 | (c & 0x3F), sb);
+            } else {
+                // three byte
+                hexEncode(0xF0 | (c >>> 12), sb);
+                hexEncode(0x80 | ((c >>> 6) & 0x3F), sb);
+                hexEncode(0x80 | (c & 0x3F), sb);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Add the byte in #xx hex format for URL Encoding
+     * 
+     * @param i
+     * @param sb 
+     */
+    private static void hexEncode(int i, StringBuffer sb) {
+        final String s = Integer.toHexString(i);
+
+        sb.append('%');
+        if (s.length() == 1) {
+            sb.append('0');
+        }
+        sb.append(s);
     }
 }
