a84598b924ca5f5a82fb3c4fb825a88dc445bdd6
Paul Houghton <paul.houghton@futurice.com>
2012-12-11 12:40:11 +0200

    Geocode example update

--- CachedGeocodeExample/src/geocodeexample/HelloMIDlet.java
+++ CachedGeocodeExample/src/geocodeexample/HelloMIDlet.java
@@ -172,7 +172,7 @@ public class HelloMIDlet extends TantalumMIDlet implements CommandListener {
                  * user wants to see the result of their latest action as soon as possible
                  * without the UI ever locking up.
                  */
-                this.locationsCache.get(getGeocodeUrl(this.getAddressTextField().getString().trim().toLowerCase()), new UITask() {
+                this.locationsCache.get(getGeocodeUrl(this.getAddressTextField().getString().trim().toLowerCase()), Worker.HIGH_PRIORITY, StaticWebCache.GET_ANYWHERE, new UITask() {
                     protected void onPostExecute(Object result) {
                         // UI Thread callback on success
                         HelloMIDlet.this.getLocationStringItem().setText((String) result);
@@ -182,7 +182,7 @@ public class HelloMIDlet extends TantalumMIDlet implements CommandListener {
                         // UI Thread callback if not already cached and the HTTP GET fails
                         HelloMIDlet.this.getLocationStringItem().setText("Service not available");
                     }
-                }, Worker.HIGH_PRIORITY, StaticWebCache.GET_ANYWHERE);
+                });
             }//GEN-BEGIN:|7-commandAction|5|7-postCommandAction
         }//GEN-END:|7-commandAction|5|7-postCommandAction
         // write post-action user code here
