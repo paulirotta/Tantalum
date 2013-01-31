793cf51a7ab34f624039e1c8695d7d107ee7f58c
bandarap <none@none>
2013-01-31 15:18:59 +0200

    Tantalum folder structure update

---
+++ TantalumUnitTests/src/org/tantalum/tests/RSSModelTest.java
@@ -0,0 +1,106 @@
+/*
+ Copyright © 2012 Paul Houghton and Futurice on behalf of the Tantalum Project.
+ All rights reserved.
+
+ Tantalum software shall be used to make the world a better place for everyone.
+
+ This software is licensed for use under the Apache 2 open source software license,
+ http://www.apache.org/licenses/LICENSE-2.0.html
+
+ You are kindly requested to return your improvements to this library to the
+ open source community at http://projects.developer.nokia.com/Tantalum
+
+ The above copyright and license notice notice shall be included in all copies
+ or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+package org.tantalum.tests;
+
+import jmunit.framework.cldc11.AssertionFailedException;
+import jmunit.framework.cldc11.TestCase;
+
+import org.tantalum.net.xml.RSSModel;
+import org.tantalum.util.StringUtils;
+import org.xml.sax.SAXException;
+
+/**
+ * Unit tests for RSSModel
+ *
+ * @author phou
+ */
+public class RSSModelTest extends TestCase {
+
+    byte[] xml;
+
+    /**
+     * RSSModel unit tests
+     */
+    public RSSModelTest() {
+        //The first parameter of inherited constructor is the number of test cases
+        super(1, "RSSModelTest");
+    }
+
+    /**
+     * Invoke unit tests by number
+     * 
+     * @param testNumber
+     * @throws Throwable 
+     */
+    public void test(int testNumber) throws Throwable {
+        xml = StringUtils.readBytesFromJAR("/rss.xml");
+        switch (testNumber) {
+            case 0:
+                testParseElement();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Test of testParseElement method, of class RSSModel.
+     *
+     * @throws AssertionFailedException
+     */
+    public void testParseElement() throws AssertionFailedException {
+        System.out.println("parseElement");
+        RSSModel instance = new RSSModel(100);
+        try {
+            instance.setXML(xml);
+        } catch (Exception ex) {
+            fail("Can not parse RSS: " + ex);
+        }
+        assertEquals("rss size", 86, instance.size());
+        try {
+            instance.setXML(null);
+            fail("Should not attempt to parse null RSS");
+        } catch (IllegalArgumentException e) {
+            // Correct answer
+        } catch (Exception ex) {
+            fail("Can not parse null RSS: " + ex);
+        }
+        try {
+            instance.setXML(new byte[0]);
+            fail("Should not handle 0 byte RSS");
+        } catch (IllegalArgumentException e) {
+            // Correct answer
+        } catch (Exception ex) {
+            fail("Can not handle 0 byte RSS: " + ex);
+        }
+        try {
+            instance.setXML(new byte[1]);
+            fail("Should not handle 1 byte RSS");
+        } catch (SAXException ex) {
+            // Correct
+        } catch (Exception e) {
+            fail("Wrong exception on parse bad 1 byte RSS: " + e);
+        }
+    }
+}
---
+++ TantalumUnitTests/src/org/tantalum/tests/StringUtilsTest.java
@@ -0,0 +1,131 @@
+/*
+ Copyright © 2012 Paul Houghton and Futurice on behalf of the Tantalum Project.
+ All rights reserved.
+
+ Tantalum software shall be used to make the world a better place for everyone.
+
+ This software is licensed for use under the Apache 2 open source software license,
+ http://www.apache.org/licenses/LICENSE-2.0.html
+
+ You are kindly requested to return your improvements to this library to the
+ open source community at http://projects.developer.nokia.com/Tantalum
+
+ The above copyright and license notice notice shall be included in all copies
+ or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+package org.tantalum.tests;
+
+
+import java.util.Vector;
+import javax.microedition.lcdui.Font;
+
+import org.tantalum.util.StringUtils;
+
+import jmunit.framework.cldc11.*;
+
+/**
+ * Unit tests for StringUtils
+ * 
+ * @author phou
+ */
+public class StringUtilsTest extends TestCase {
+    
+    /**
+     * Create a new test harness
+     */
+    public StringUtilsTest() {
+        //The first parameter of inherited constructor is the number of test cases
+        super(4,"StringUtilsTest");
+    }            
+
+    /**
+     * Test invocation by number
+     * 
+     * @param testNumber
+     * @throws Throwable 
+     */
+    public void test(int testNumber) throws Throwable {
+        switch (testNumber) {    
+            case 0:
+                testReadStringFromJAR();
+                break;
+            case 1:
+                testReadBytesFromJAR();
+                break;
+            case 2:
+                testTruncate();
+                break;
+            case 3:
+                testSplitToLines();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Test of testReadStringFromJAR method, of class StringUtils.
+     * 
+     * @throws AssertionFailedException
+     * @throws Exception 
+     */
+    public void testReadStringFromJAR() throws AssertionFailedException, Exception {
+        System.out.println("readStringFromJAR");
+        String name_1 = "";
+        String expResult_1 = "";
+        String result_1 = StringUtils.readStringFromJAR(name_1);
+        assertEquals(expResult_1, result_1);
+    }
+
+    /**
+     * Test of testReadBytesFromJAR method, of class StringUtils.
+     * 
+     * @throws AssertionFailedException
+     * @throws Exception 
+     */
+    public void testReadBytesFromJAR() throws AssertionFailedException, Exception {
+        System.out.println("readBytesFromJAR");
+        byte[] result_1 = StringUtils.readBytesFromJAR("/rss.xml");
+        assertNotEquals(0, result_1.length);
+    }
+
+    /**
+     * Test of testTruncate method, of class StringUtils.
+     * 
+     * @throws AssertionFailedException 
+     */
+    public void testTruncate() throws AssertionFailedException {
+        System.out.println("truncate");
+        String str_1 = "This is a a really long line of text--------------------------------------------------------------------------------------------------------------------------------";
+        Font font_1 = Font.getDefaultFont();
+        int maxWidth_1 = 240;
+        String expResult_1 = "This is a a really long line of t...";
+        String result_1 = StringUtils.truncate(str_1, font_1, maxWidth_1);
+        System.out.println(expResult_1);
+        System.out.println(result_1);
+        assertEquals(result_1, expResult_1);
+    }
+
+    /**
+     * Test of testSplitToLines method, of class StringUtils.
+     * 
+     * @throws AssertionFailedException 
+     */
+    public void testSplitToLines() throws AssertionFailedException {
+        System.out.println("splitToLines");
+        Vector vector_1 = new Vector();
+        String text_1 = "This is a a really long line of text";
+        Font font_1 = Font.getDefaultFont();
+        int maxWidth_1 = 100;
+        StringUtils.splitToLines(vector_1, text_1, font_1, maxWidth_1);
+        this.assertEquals(3, vector_1.size());
+    }
+}
