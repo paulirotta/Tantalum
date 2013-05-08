3234d91de8ca69fa2f8f9f42586c7cdc9c58c59d
Paul Houghton <paulirotta@gmail.com>
2013-05-08 18:24:50 +0200

    Deleted deprecated Android and java.net stubs- JavaDoc is now cleaner

--- DEPRECATED_AndroidStubs/src/android/graphics/Bitmap.java
+++
@@ -1,7 +0,0 @@
-package android.graphics;
-
-public class Bitmap {
-    public static Bitmap createScaledBitmap(Bitmap source, int width, int height, boolean filter) {
-        return source;
-    }
-}
--- DEPRECATED_AndroidStubs/src/android/graphics/BitmapFactory.java
+++
@@ -1,8 +0,0 @@
-package android.graphics;
-
-public class BitmapFactory {
-
-    static public Bitmap decodeByteArray(byte[] data, int offset, int length) {
-        return null;
-    }
-}
