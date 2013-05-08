50fe1c044aa933e4e5a843ac873aeeffd57a9b34
Paul Houghton <paulirotta@gmail.com>
2013-05-08 14:00:00 +0200

    Added CryptoUtilsTest- no problems found yet

---
+++ TantalumUnitTests/src/test/java/org/tantalum/util/CryptoUtilsTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tantalum.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.DigestException;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.tantalum.MockedStaticInitializers;
+
+/**
+ *
+ * @author phou
+ */
+public class CryptoUtilsTest extends MockedStaticInitializers {
+    String imageUrl1 = "http://1.maps.nlp.nokia.com/maptile/2.1/maptile/newest/normal.day/11/1099/675/128/png8?app_id=Db84BEVKmbJGsBiRR_RJ&token=FzPD8qiiOKt9ZDmy7jCtEQ&";
+    String imageUrl3 = "http://2.maps.nlp.nokia.com/maptile/2.1/maptile/newest/normal.day/11/1105/674/128/png8?app_id=Db84BEVKmbJGsBiRR_RJ&token=FzPD8qiiOKt9ZDmy7jCtEQ&";
+    String imageUrl2 = "http://1.maps.nlp.nokia.com/maptile/2.1/maptile/newest/normal.day/11/1098/675/128/png8?app_id=Db84BEVKmbJGsBiRR_RJ&token=FzPD8qiiOKt9ZDmy7jCtEQ&";
+    String boodle = "boodle";
+    static CryptoUtils cryptoUtils;
+
+    @Before
+    public final void getSingleton() {
+        cryptoUtils = CryptoUtils.getInstance();
+    }
+
+    @Test
+    public void boodleLongsAreNotTheSameTest() throws DigestException, UnsupportedEncodingException {
+        assertNotEquals("boodle is not a url", cryptoUtils.toDigest(imageUrl1), cryptoUtils.toDigest(boodle));
+    }
+
+    @Test
+    public void almostSameLongsAreNotTheSameTest() throws DigestException, UnsupportedEncodingException {
+        assertNotEquals(cryptoUtils.toDigest(imageUrl1), cryptoUtils.toDigest(imageUrl2));
+    }
+
+    // These values might produce same long according to system tests (verifying...)
+    @Test
+    public void magicValuesAreNotTheSameTest() throws DigestException, UnsupportedEncodingException {
+        assertNotEquals(cryptoUtils.toDigest(imageUrl1), cryptoUtils.toDigest(imageUrl3));
+    }
+
+    @Test
+    public void spaceIsNotEqualEmptyStringTest() throws DigestException, UnsupportedEncodingException {
+        assertNotEquals(cryptoUtils.toDigest(" "), cryptoUtils.toDigest(""));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullStringTest() throws DigestException, UnsupportedEncodingException {
+        cryptoUtils.toDigest((String) null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullByteArrayTest() throws DigestException, UnsupportedEncodingException {
+        cryptoUtils.toDigest((byte[]) null);
+    }    
+}
