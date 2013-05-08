b22ab1477b543ae75e5a61deeca723b57a9c64d6
Paul Houghton <paulirotta@gmail.com>
2013-05-08 14:06:45 +0200

    Added unit tests for crypto, all pass

--- TantalumUnitTests/src/test/java/org/tantalum/util/CryptoUtilsTest.java
+++ TantalumUnitTests/src/test/java/org/tantalum/util/CryptoUtilsTest.java
@@ -16,6 +16,7 @@ import org.tantalum.MockedStaticInitializers;
  * @author phou
  */
 public class CryptoUtilsTest extends MockedStaticInitializers {
+
     String imageUrl1 = "http://1.maps.nlp.nokia.com/maptile/2.1/maptile/newest/normal.day/11/1099/675/128/png8?app_id=Db84BEVKmbJGsBiRR_RJ&token=FzPD8qiiOKt9ZDmy7jCtEQ&";
     String imageUrl3 = "http://2.maps.nlp.nokia.com/maptile/2.1/maptile/newest/normal.day/11/1105/674/128/png8?app_id=Db84BEVKmbJGsBiRR_RJ&token=FzPD8qiiOKt9ZDmy7jCtEQ&";
     String imageUrl2 = "http://1.maps.nlp.nokia.com/maptile/2.1/maptile/newest/normal.day/11/1098/675/128/png8?app_id=Db84BEVKmbJGsBiRR_RJ&token=FzPD8qiiOKt9ZDmy7jCtEQ&";
@@ -37,6 +38,19 @@ public class CryptoUtilsTest extends MockedStaticInitializers {
         assertNotEquals(cryptoUtils.toDigest(imageUrl1), cryptoUtils.toDigest(imageUrl2));
     }
 
+    @Test
+    public void statelessSequenceTest() throws DigestException, UnsupportedEncodingException {
+        long l = cryptoUtils.toDigest(imageUrl2);
+        cryptoUtils.toDigest("wahhh");
+        assertEquals(l, cryptoUtils.toDigest(imageUrl2));
+    }
+
+    // These values might produce same long according to system tests (verifying...)
+    @Test
+    public void magicValueTest() throws DigestException, UnsupportedEncodingException {
+        assertEquals(1683574533465955905l, cryptoUtils.toDigest(imageUrl3));
+    }
+
     // These values might produce same long according to system tests (verifying...)
     @Test
     public void magicValuesAreNotTheSameTest() throws DigestException, UnsupportedEncodingException {
@@ -56,5 +70,5 @@ public class CryptoUtilsTest extends MockedStaticInitializers {
     @Test(expected = IllegalArgumentException.class)
     public void nullByteArrayTest() throws DigestException, UnsupportedEncodingException {
         cryptoUtils.toDigest((byte[]) null);
-    }    
+    }
 }
