c5ee9d82678423e2a96ebe3fd08a1870d3a23c09
Kai Inkinen <kai.inkinen@futurice.com>
2013-06-12 09:08:40 +0200

    Removed the ignored test

--- TantalumJME/src/test/java/org/tantalum/jme/RMSKeyUtilsTest.java
+++ TantalumJME/src/test/java/org/tantalum/jme/RMSKeyUtilsTest.java
@@ -103,35 +103,6 @@ public class RMSKeyUtilsTest extends RMSTestUtils {
         }
     }
 
-
-    @Ignore
-    @Test
-    public void hashValueCollision() {
-        final int keyIndex = 2;
-        final int valueIndex = 2;
-        final Long hashValue = 8589934594l;
-
-        assertEquals(hashValue, keyUtils.toIndexHash(keyIndex, valueIndex));
-
-
-        final int numberOfEntries = 500;
-        Set<Long> values = new HashSet<Long>(numberOfEntries * numberOfEntries);
-        int counter = 0;
-
-        for (int key = 1; key < numberOfEntries; key++) {
-            for (int value = 1; value < numberOfEntries; value++) {
-                Long hash = keyUtils.toIndexHash(key, value);
-                if (values.contains(hash)) {
-                    System.out.println("Hash value " + hash + " already exists");
-                }
-
-                values.add(hash);
-            }
-        }
-
-        assertEquals((numberOfEntries - 1) * (numberOfEntries - 1), values.size());
-    }
-
     @Test
     public void collidingHashValues() {
         Long l1 = -6745443400823342501l;
