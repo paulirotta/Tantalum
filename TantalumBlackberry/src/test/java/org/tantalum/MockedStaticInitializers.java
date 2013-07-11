343e8d319b94f3c506ec597a608ca4065666ea89
Paul Houghton <paulirotta@gmail.com>
2013-07-11 11:16:06 +0200

    Added TantalumBlackberry directory to mirror new package scheme
    This is a first step to merging in adiksonline's Blackberry port. The
    build structure and other aspects of Tantalum have evolved since the port
    was forked 3 months ago. The following steps will be to copy by hand key
    parts of the TantalumLibrary directory structure relating to BB into the
    TantalumBlackberry directory, update them as needed to reflect lib changes
    or caching. The new RMSFastCache in the JME port (the TantalumBlackberry
    directory is currently just a copy of the TantalumJME directory with minor
    build changes) should also work on
    Blackberry, perhaps unchanged if luck and design and standards hold.

---
+++ TantalumBlackberry/src/test/java/org/tantalum/MockedStaticInitializers.java
@@ -0,0 +1,84 @@
+/*
+ Copyright (c) 2012-2013 Nokia Corporation. All rights reserved.
+ Nokia and Nokia Connecting People are registered trademarks of Nokia Corporation.
+ Oracle and Java are trademarks or registered trademarks of Oracle and/or its
+ affiliates. Other product and company names mentioned herein may be trademarks
+ or trade names of their respective owners.
+
+ Tantalum software shall be used to make the world a better place for everyone.
+
+ This software is licensed for use under the Apache 2 open source software license,
+ http://www.apache.org/licenses/LICENSE-2.0.html
+
+ You are kindly requested to return your improvements to this library to the
+ open source community at http://projects.developer.nokia.com/Tantalum or 
+ http://github.com/TantalumMobile
+
+ The above copyright and license notice notice shall be included in all copies
+ or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+package org.tantalum;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.tantalum.util.L;
+import org.tantalum.util.SystemOutLogger;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * Base class for tests requiring Tantalum methods to be mocked out.
+ * <p/>
+ * The RunWith-annotation uses the PowerMockRunner to execute tests. This allows
+ * us to mock static methods etc.
+ * <p/>
+ * The PrepareForTest-annotation is mark any classes that need static mocking
+ * <p/>
+ * The SuppressStaticInitalizationFor-annotation turns off any static
+ * initalizing code in the provided classes. This way we can avoid running
+ * potentially difficult to test platform-dependant code.
+ * <p/>
+ * To use this class in tests, just extend it. If some other Tantalum-classes
+ * need to be mocked out, add them here.
+ *
+ * @author Kai Inkinen <kai.inkinen@futurice.com>
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({L.class, PlatformUtils.class})
+@SuppressStaticInitializationFor(
+        {"org.tantalum.util.L",
+                "org.tantalum.PlatformUtils",
+                "org.tantalum.storage.StaticCache",
+                "org.tantalum.net.StaticWebCache"})
+public abstract class MockedStaticInitializers {
+
+    // Declare a publicly available mock for all the subclasses to use
+    public PlatformUtils platformUtils;
+    public L log;
+
+    @Before
+    public final void tantalumMockTestFixture() {
+        PowerMockito.mockStatic(PlatformUtils.class);
+        PowerMockito.mockStatic(L.class);
+
+        platformUtils = mock(PlatformUtils.class);
+        when(PlatformUtils.getInstance()).thenReturn(platformUtils);
+
+        log = new SystemOutLogger();
+        when(platformUtils.getLog()).thenReturn(log);
+    }
+}
---
+++ TantalumBlackberry/src/test/java/org/tantalum/bb/RMSKeyUtilsTest.java
@@ -0,0 +1,122 @@
+package org.tantalum.jme;
+
+/**
+ * User: kink
+ * Date: 2013.05.31
+ * Time: 01:08
+ */
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Unit tests for the key methods in the RMSFastCache.
+ */
+public class RMSKeyUtilsTest {
+
+    RMSKeyUtils keyUtils;
+
+    @Before
+    public final void rmsFastCachedTestFixture() throws Exception {
+        keyUtils = new RMSKeyUtils();
+    }
+
+    @Test
+    public void indexKeyForTrivialValueIsReversible() {
+        long indexHash = keyUtils.toIndexHash(13, 0);
+        assertEquals(13, keyUtils.toKeyIndex(indexHash));
+    }
+
+    @Test
+    public void indexValueForTrivialValIsReversible() {
+        long indexHash = keyUtils.toIndexHash(0, 97);
+        assertEquals(97, keyUtils.toValueIndex(indexHash));
+    }
+
+    @Test
+    public void reversible() {
+        final int value = 1 << 16;
+
+        long indexHash = keyUtils.toIndexHash(value, value);
+
+        assertEquals(value, keyUtils.toKeyIndex(indexHash));
+        assertEquals(value, keyUtils.toValueIndex(indexHash));
+    }
+
+
+    @Test
+    public void indexKeyCanReverseAllIntegerBits() {
+        int one = 1;
+        for (int bits = 0; bits < 32; bits++) {
+            final int shifted = one << bits;
+            long indexHash = keyUtils.toIndexHash(shifted, 0);
+            assertEquals("For shift value " + bits + " values are not equal", shifted, keyUtils.toKeyIndex(indexHash));
+        }
+    }
+
+    @Test
+    public void indexValueCanReverseAllIntegerBits() {
+        int one = 1;
+        for (int bits = 0; bits < 32; bits++) {
+            final int shifted = one << bits;
+            long indexHash = keyUtils.toIndexHash(0, shifted);
+            assertEquals("For shift value " + bits + " values are not equal", shifted, keyUtils.toValueIndex(indexHash));
+        }
+    }
+
+    @Test
+    public void valueIndexValuesCanStoreEntireIntegerRange() {
+        long one = 1;
+        long previous = 0;
+
+        for (int bits = 0; bits < 32; bits++) {
+            long shifted = one << bits;
+            assertTrue("Overflow when shifting long. Previous value: " + previous + " now: " + shifted, shifted > previous);
+            previous = shifted;
+
+            int vi = keyUtils.toValueIndex(shifted);
+
+            assertEquals((int) shifted, vi);
+        }
+    }
+
+    @Test
+    public void keyIndexValuesCanStoreEntireIntegerRange() {
+        final int bitsInInteger = 32;
+        final long one = 1;
+
+        for (int bits = 0; bits < bitsInInteger; bits++) {
+            final int keyIntegerValue = (int)(one << bits);
+            final long shiftedLong = one << (bits + bitsInInteger);
+
+            int ki = keyUtils.toKeyIndex(shiftedLong);
+
+            assertEquals(keyIntegerValue, ki);
+        }
+    }
+
+    @Test
+    public void collidingHashValues() {
+        Long l1 = -6745443400823342501l;
+        Long l2 = -1687905435239235501l;
+
+        final int l1KeyIndex = keyUtils.toKeyIndex(l1);
+        final int l2KeyIndex = keyUtils.toKeyIndex(l2);
+        assertNotEquals(l1KeyIndex, l2KeyIndex);
+
+        final int l1ValueIndex = keyUtils.toValueIndex(l1);
+        final int l2ValueIndex = keyUtils.toValueIndex(l2);
+        assertNotEquals(l1ValueIndex, l2ValueIndex);
+    }
+
+}
+
+
