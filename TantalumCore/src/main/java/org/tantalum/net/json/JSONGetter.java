9b54fbea98978a272c1a2481795b31b18cf00e21
Pekka Kauppila <pekka.kauppila@futurice.com>
2013-08-23 15:20:09 +0300

    Fix charset name when constructing a String in JSONGetter

--- TantalumCore/src/main/java/org/tantalum/net/json/JSONGetter.java
+++ TantalumCore/src/main/java/org/tantalum/net/json/JSONGetter.java
@@ -85,7 +85,7 @@ public class JSONGetter extends HttpGetter {
         String value = null;
         
         try {
-            value = new String(((LOR) super.exec(in)).getBytes(), "UTF8").trim();
+            value = new String(((LOR) super.exec(in)).getBytes(), "UTF-8").trim();
             if (value.startsWith("[")) {
                 // Parser expects non-array base object- add one
                 value = "{\"base:\"" + value + "}";
